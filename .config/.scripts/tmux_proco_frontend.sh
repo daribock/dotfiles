10d9581b5855dfef698b22a5cb3aab36c76b94e0
Darius Kletter <darikletter@gmail.com>
2025-02-12 12:35:32 +0100

    :closed_lock_with_key: Update gpg keys

--- .config/.scripts/tmux_proco_frontend.sh
+++ .config/.scripts/tmux_proco_frontend.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
-#
-# Setup a work space called `proco-frontend` with two windows
-# C-m acts like enter key in tmux
-#
+
+# Setup a workspace called `proco-frontend` with specific windows
 session="proco-frontend"
 
-# Create new tmux sesseion called proco-frontend
-tmux new-session -d -s $session -n run
+# Check if the tmux session already exists
+if tmux has-session -t $session 2>/dev/null; then
+    echo "Session $session already exists. Attaching..."
+else
+    echo "Creating session $session..."
 
-# Change directory
-tmux send-keys 'cd ~/dev/mediamarktsaturn/proco-frontend/ui; pnpm install; pnpm start' C-m
+    # Create new tmux session
+    tmux new-session -d -s $session -n run
 
-# Split window vertically
-tmux split-window
+    # Setup the first window (run)
+    tmux send-keys -t $session:0 'cd ~/dev/mediamarktsaturn/proco-frontend/ui; pnpm install; pnpm start' C-m
 
-# Change directory
-tmux send-keys 'cd ~/dev/mediamarktsaturn/proco-local-env; podman container prune -f; podman image prune -a -f; podman volume prune -f; make run' C-m
+    # Split the first window vertically
+    tmux split-window -t $session:0
 
-# Create new window to work in
-tmux new-window -t $session:1 -n work
+    # Setup the split window commands
+    tmux send-keys -t $session:0.1 'cd ~/dev/mediamarktsaturn/proco-local-env; podman container prune -f; podman image prune -a -f; podman volume prune -f; make run' C-m
 
-# Fetch changes from git and open vscode
-tmux send-keys 'cd ~/dev/mediamarktsaturn/proco-frontend; git fetch;  code .' C-m
+    # Create a second window (work)
+    tmux new-window -t $session:1 -n work
 
-# Finally attach to temux session
+    # Setup the second window commands
+    tmux send-keys -t $session:1 'cd ~/dev/mediamarktsaturn/proco-frontend; git fetch; code .' C-m
+fi
+
+# Attach to the tmux session
 tmux attach-session -t $session
+
